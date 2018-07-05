894bc3896ff0ed3de5dd636d0f1c7037904444a9
xvjunjie <1559643591@qq.com>
2018-07-05 19:28:03 +0800

    .

--- demo4/baidutieba_3.py
+++ demo4/baidutieba_3.py
@@ -15,46 +15,38 @@ class TiebaSpider():
     def parse_url(self, url):
         '''获取页面内容'''
         response = requests.get(url, headers=self.headers)
-        return response.content.decode()
+        return response.content
 
-    def get_content_list(self,html_str):
+    def get_content_list(self, html_str):
         '''提取页面数据'''
-        html = etree.Element(html_str)
-        # html = etree.HTML(html_str)
-        div_list = html.xpath("//div[contains(@class,'i')]") #提取div列表进行分组
+        print(html_str)
 
-        content_list =[]
+        html = etree.HTML(html_str)
+        div_list = html.xpath("//div[contains(@class,'i')]")  # 提取div列表进行分组
+
+        content_list = []
         for div in div_list:
-            item={}
+            item = {}
             # 获取贴吧标题
-            item["title"] = div.xpath("./a/text()")[0] if len(div.xpath("./a/text()"))>0 else None
+            item["title"] = div.xpath("./a/text()")[0] if len(div.xpath("./a/text()")) > 0 else None
             # 获取标题对应的详情地址
-            item["href"] = self.part_url+div.xpath(".a/@href")[0] if len(div.xpath("./a/@href"))>0 else None
+            item["href"] = self.part_url + div.xpath(".a/@href")[0] if len(div.xpath("./a/@href")) > 0 else None
 
-            item["img_list"] = self.get_img_list(item["href"],[])
+            item["img_list"] = self.get_img_list(item["href"], [])
 
             content_list.append(item)
             print(content_list)
 
-
-
-
-
-
-    def get_img_list(self,detail_url,total_img_list):
+    def get_img_list(self, detail_url, total_img_list):
         '''获取'''
         detail_htlm_str = self.part_url(detail_url)
-        detail_html = etree.Element(detail_htlm_str)
+        detail_html = etree.HTML(detail_htlm_str)
 
         img_list = detail_html.xpath("img[@class='BDE_Image']/@src")
         total_img_list.extend(img_list)
         print(total_img_list)
 
-
-
         return total_img_list
-
-
 
     def run(self):
         # 1.获取页面返回的内容html
@@ -62,11 +54,7 @@ class TiebaSpider():
         # 3.使用xpath获取要抓取的字段数据
 
         html_str = self.parse_url(self.url_temp)
-        content_list  = self.get_content_list(html_str)
-
-
-
-
+        content_list = self.get_content_list(html_str)
 
 
 if __name__ == '__main__':
